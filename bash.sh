136a97602f1a0e1fc09c1d1c9a127594de90cd82
helpless69 <104577370+helpless69@users.noreply.github.com>
2024-10-04 16:21:46 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Encode video using av1an with SVT-AV1 encoder parameters.
-
 # Step 1, make test clip and compare size.
 ffmpeg -hide_banner -i video.mkv -c copy -t 65 input.mkv && ls -sh
 
+# Step 2 encode video using av1an with SVT-AV1-PSY encoder parameters.
 av1an -i input.mkv -e svt-av1 \
 --photon-noise 2 \
 --vmaf \
@@ -39,7 +38,35 @@ av1an -i input.mkv -e svt-av1 \
 -c mkvmerge \
 -o Output.mkv
 
+# Encode 2
+av1an -i input.mkv -e svt-av1 \
+--photon-noise 2 \
+--vmaf \
+-w 2 \
+--set-thread-affinity 2 \
+-m lsmash \
+-v ' --crf 30 \
+--preset 6 \
+--frame-luma-bias 60 \
+--variance-boost-strength 3 \
+--variance-octile 6 \
+--sharpness 4 \
+--enable-dlf 2 \
+--enable-tf 0 \
+--keyint 240 \
+--lookahead 120 \
+--tune 3 \
+--enable-qm 1 \
+--lp 2 \
+--qm-min 0 \
+--qm-max 15 \
+--enable-cdef 0 \
+--irefresh-type 1 \
+--color-primaries 1 \
+--transfer-characteristics 1 \
+--matrix-coefficients 1 ' \
+-c mkvmerge \
+-o Output1.mkv
+
 # Remove test clip to save upload bandwidth.
 rm input.mkv
-# Add more encoding commands below if needed, e.g., for more videos
-# av1an -i video2.mkv -e svt-av1 --crf 30 ... -o Output2.mkv
