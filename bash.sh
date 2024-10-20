a771915981d80b9c4b1e487221ec93e444b5fc6f
helpless69 <104577370+helpless69@users.noreply.github.com>
2024-10-20 14:46:22 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
 # Step 1, make test clip and compare size.
-# ffmpeg -hide_banner -i video.mkv -c copy -t 65 input.mkv && ls -sh
+ffmpeg -hide_banner -i video.mp4 -c copy -t 300 input.mkv && ls -sh
 
 # Step 2 encode video using av1an with SVT-AV1-PSY encoder parameters.
-av1an -i video.mkv -e svt-av1 \
+av1an -i input.mkv -e svt-av1 \
 --photon-noise 2 \
 --vmaf \
 -w 2 \
 --set-thread-affinity 2 \
 -m lsmash \
--v ' --crf 30 \
+-v ' --crf 24 \
 --preset 6 \
 --frame-luma-bias 60 \
---variance-boost-strength 3 \
---variance-octile 6 \
+--variance-boost-strength 1 \
+--variance-octile 5 \
+--scd 0 \
 --sharpness 4 \
---enable-dlf 2 \
 --enable-tf 0 \
 --keyint 240 \
---lookahead 120 \
+--lookahead 60 \
 --tune 3 \
---enable-qm 1 \
 --lp 2 \
---qm-min 0 \
+--qm-min 1 \
 --qm-max 15 \
 --enable-cdef 0 \
 --irefresh-type 1 \
@@ -33,7 +32,67 @@ av1an -i video.mkv -e svt-av1 \
 --chroma-qm-min 10 \
 --chroma-qm-max 15 ' \
 -c mkvmerge \
--o Output.mkv
+-o p6crf24.mkv
+
+av1an -i input.mkv -e svt-av1 \
+--photon-noise 2 \
+--vmaf \
+-w 2 \
+--set-thread-affinity 2 \
+-m lsmash \
+-v ' --crf 26 \
+--preset 6 \
+--frame-luma-bias 60 \
+--variance-boost-strength 1 \
+--variance-octile 5 \
+--scd 0 \
+--sharpness 4 \
+--enable-tf 0 \
+--keyint 240 \
+--lookahead 60 \
+--tune 3 \
+--lp 2 \
+--qm-min 1 \
+--qm-max 15 \
+--enable-cdef 0 \
+--irefresh-type 1 \
+--color-primaries 1 \
+--transfer-characteristics 1 \
+--matrix-coefficients 1 \
+--chroma-qm-min 10 \
+--chroma-qm-max 15 ' \
+-c mkvmerge \
+-o p6crf26.mkv
+
+av1an -i input.mkv -e svt-av1 \
+--photon-noise 2 \
+--vmaf \
+-w 2 \
+--set-thread-affinity 2 \
+-m lsmash \
+-v ' --crf 26 \
+--preset 4 \
+--frame-luma-bias 60 \
+--variance-boost-strength 1 \
+--variance-octile 5 \
+--scd 0 \
+--sharpness 4 \
+--enable-tf 0 \
+--keyint 240 \
+--lookahead 60 \
+--tune 3 \
+--lp 2 \
+--qm-min 1 \
+--qm-max 15 \
+--enable-cdef 0 \
+--irefresh-type 1 \
+--color-primaries 1 \
+--transfer-characteristics 1 \
+--matrix-coefficients 1 \
+--chroma-qm-min 10 \
+--chroma-qm-max 15 ' \
+-c mkvmerge \
+-o p4crf26.mkv
 
 # Encode 2 --adaptive-film-grain 1 \
 #          --film-grain 4 \
