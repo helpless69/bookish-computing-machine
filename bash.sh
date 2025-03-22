6d21ca2612694a014e35ef63b1c3de078baf6e4d
helpless69 <104577370+helpless69@users.noreply.github.com>
2025-03-22 16:03:27 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
 
 # Step 1, Download Video.
-curl "https://share.secretz.workers.dev/2:/mimk138-002.mkv" -o video.mkv
+curl "https://share.secretz.workers.dev/2:/video.mkv" -o video.mkv
 
 # Step 2, make test clip and compare size.
 # ffmpeg -hide_banner -i video.mp4 -c copy -t 300 input.mkv && ls -sh
 
 # Step 3 encode video using av1an with SVT-AV1-PSY encoder parameters.
 av1an -i video.mkv -e svt-av1 \
---photon-noise 2 \
---vmaf \
+--pix-format yuv420p10le \
 -w 2 \
 --set-thread-affinity 2 \
--m lsmash \
 -v ' --crf 28 \
 --preset 6 \
---frame-luma-bias 60 \
---variance-boost-strength 1 \
+--frame-luma-bias 50 \
+--variance-boost-strength 2 \
 --variance-octile 5 \
 --scd 0 \
---sharpness 4 \
---enable-tf 0 \
---keyint 240 \
+--sharpness 1 \
+--enable-tf 2 \
+--keyint -1 \
 --lookahead 60 \
---tune 3 \
---lp 2 \
+--tune 2 \
 --qm-min 1 \
 --qm-max 15 \
---enable-cdef 0 \
---irefresh-type 1 \
+--enable-cdef 1 \
+--irefresh-type 2 \
 --color-primaries 1 \
 --transfer-characteristics 1 \
 --matrix-coefficients 1 \
 --chroma-qm-min 10 \
 --chroma-qm-max 15 ' \
--c mkvmerge \
--o 02.mkv
+-o encode1.mkv
 
 
 # Encode 2 --adaptive-film-grain 1 \
